518ae78ade6d93fbb444e6c3684175020c1e627e
Victor Vang <vv.fitness614@gmail.com>
2022-01-27 18:10:36 -0500

    material from module 1/09 worked on

---
+++ module-1/09_Classes_and_Encapsulation/lecture/PetInfo/Pet.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetInfo
+{
+    public class Pet
+    {
+        public Pet()
+        {
+
+        }
+
+        public Pet (string petName)
+        {
+            this.petName = petName;
+        }
+
+        public string petName = "Default Dog";
+
+        public string PetType { get; set; }
+        public int PetAge { get; set; }
+
+        public string Color { get; private set; } = "brown";
+
+        public void SetColor(string newColor)
+        {
+            Color = newColor;
+        }
+
+        private string eyeColor = "";
+        public string EyeColor
+        {
+            get
+            {
+                return eyeColor;
+            }
+            set
+            {
+                if (value == "Black" || value == "Brown")
+                {
+                    eyeColor = value;
+                }
+            }
+        }
+        public string DisplayInfo()
+        {
+            return $"{petName} - {PetType} - {PetAge} - {Color} - {EyeColor}";
+        }
+    }
+}
