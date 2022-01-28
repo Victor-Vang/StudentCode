46b8eda450984af192f4d21ecf5a7a532c3af212
Victor Vang <vv.fitness614@gmail.com>
2022-01-28 09:03:09 -0500

    pulled down and viewed module 1-09

--- module-1/09_Classes_and_Encapsulation/exercise/Exercises/Classes/FruitTree.cs
+++ module-1/09_Classes_and_Encapsulation/exercise/Exercises/Classes/FruitTree.cs
@@ -3,6 +3,20 @@
     public class FruitTree
     {
 
+        public FruitTree()
+        {
 
+        }
+
+        public FruitTree(string typeOfFruit, int startingPiecesOfFruit)
+        {
+
+        }
+
+        public string TypeOfFruit { get; private set; }
+
+        public int PiecesOfFruitLeft { get; private set; }
+
+       
     }
 }
--- module-1/09_Classes_and_Encapsulation/exercise/Exercises/Classes/HomeworkAssignment.cs
+++ module-1/09_Classes_and_Encapsulation/exercise/Exercises/Classes/HomeworkAssignment.cs
@@ -2,6 +2,22 @@
 {
     public class HomeworkAssignment
     {
+        public HomeworkAssignment()
+        {
+
+        }
+        public HomeworkAssignment(int possibleMarks, string submitterName)
+        {
+            this.PossibleMarks = possibleMarks;
+            this.SubmitterName = submitterName;
+        }
+
+        // HomeworkAssignment assignment = new HomeworkAssignment();
+
+        public int EarnedMarks { get; set; }
+        public int PossibleMarks { get; private set; }
+        public string SubmitterName { get; private set; }
+        public string LetterGrade { get; }
 
 
     }
