6f446de5247dfea480380a7dd022d7fc2ac40859
Victor Vang <vv.fitness614@gmail.com>
2022-03-02 14:01:38 -0500

    server side API lecture work

--- module-2/13_Server_Side_APIs_Part_1/lecture/server/HotelReservationsServer/Controllers/ReservationsController.cs
+++ module-2/13_Server_Side_APIs_Part_1/lecture/server/HotelReservationsServer/Controllers/ReservationsController.cs
@@ -22,5 +22,57 @@ namespace HotelReservations.Controllers
                 reservationDao = new ReservationMemoryDao();
             }
         }
+
+        [HttpGet()]
+        public ActionResult<List<Reservation>> GetReservations()
+        {
+            return reservationDao.List();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Reservation> GetReservation(int id)
+        {
+            Reservation result = reservationDao.Get(id);
+            if (result != null)
+            {
+                return result;
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteReservation(int id)
+        {
+            reservationDao.Delete(id);
+
+            if (reservationDao.Delete(id))
+            {
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost()]
+        public ActionResult<Reservation> AddReservation(Reservation reservation)
+        {
+            Reservation result = reservationDao.Create(reservation);
+
+            if (result == null)
+            {
+                return StatusCode(500);
+            }
+            else
+            {
+                return result;
+            }
+
+           
+        }
     }
 }
