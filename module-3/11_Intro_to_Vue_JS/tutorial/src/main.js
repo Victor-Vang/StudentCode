81cfc56670d63246df20f78e982a436b1609deeb
Victor Vang <vv.fitness614@gmail.com>
2022-03-28 09:30:18 -0400

    worked on module 3/11 tutorial

--- module-3/11_Intro_to_Vue_JS/tutorial/src/main.js
+++ module-3/11_Intro_to_Vue_JS/tutorial/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
+import { component } from 'vue/types/umd';
 import App from './App.vue'
+import TodoList from './components/TodoList';
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+components: {
+  TodoList
+}
