59c03886949a42bc9430c0693f0acec6c11e4bd7
Victor Vang <vv.fitness614@gmail.com>
2022-03-30 09:26:46 -0400

    worked on module 3/11 exercise

--- module-3/11_Intro_to_Vue_JS/exercise/src/components/UserList.vue
+++ module-3/11_Intro_to_Vue_JS/exercise/src/components/UserList.vue
@@ -1,22 +1,30 @@
 <template>
   <table id="tblUsers">
     <thead>
-    <tr>
+      <tr>
         <th>First Name</th>
         <th>Last Name</th>
         <th>Username</th>
         <th>Email Address</th>
         <th>Status</th>
-    </tr>
+      </tr>
     </thead>
     <tbody>
       <tr>
-        <td><input type="text" id="firstNameFilter"/></td>
-        <td><input type="text" id="lastNameFilter"/></td>
-        <td><input type="text" id="usernameFilter"/></td>
-        <td><input type="text" id="emailFilter"/></td>
         <td>
-          <select id="statusFilter">
+          <input type="text" id="firstNameFilter" v-model="filter.firstName" />
+        </td>
+        <td>
+          <input type="text" id="lastNameFilter" v-model="filter.lastName" />
+        </td>
+        <td>
+          <input type="text" id="usernameFilter" v-model="filter.username" />
+        </td>
+        <td>
+          <input type="text" id="emailFilter" v-model="filter.emailAddress" />
+        </td>
+        <td>
+          <select id="statusFilter" v-model="filter.status">
             <option value="">Show All</option>
             <option value="Active">Active</option>
             <option value="Disabled">Disabled</option>
@@ -24,6 +32,23 @@
         </td>
       </tr>
       <!-- user listing goes here -->
+      <tr class="user" v-for="user in filteredList" v-bind:key="user.firstName">
+        <td>
+          <h4>{{ user.firstName }}</h4>
+        </td>
+        <td>
+          <h4>{{ user.lastName }}</h4>
+        </td>
+        <td>
+          <h4>{{ user.username }}</h4>
+        </td>
+        <td>
+          <h4>{{ user.emailAddress }}</h4>
+        </td>
+        <td>
+          <h4>{{ user.status }}</h4>
+        </td>
+      </tr>
     </tbody>
   </table>
 </template>
@@ -40,19 +65,41 @@ export default {
         { firstName: 'Ben', lastName: 'Carter', username: 'bcarter', emailAddress: 'bcarter@gmail.com', status: 'Active' },
         { firstName: 'Katie', lastName: 'Jackson', username: 'kjackson', emailAddress: 'kjackson@yahoo.com', status: 'Active' },
         { firstName: 'Mark', lastName: 'Smith', username: 'msmith', emailAddress: 'msmith@foo.com', status: 'Disabled' }
-      ]
+      ],
+      filter : {
+        firstName:"",
+        lastName:"",
+        username:"",
+        emailAddress:"",
+        status:""
+      },
+    };
+  },
+  computed: {
+    filteredList() {
+      return this.users.filter((item) => {
+        if (item.firstName.toLowerCase().includes(this.filter.firstName.toLowerCase())) {
+          return item;
+        }
+        // if (item.lastName.toLowerCase().includes(this.filter.lastName.toLowerCase())) {
+        //   return item;
+        // }
+      });
     }
   }
+
+  
 }
 </script>
 
 <style scoped>
 table {
   margin-top: 20px;
-  font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 }
 th {
-  text-transform: uppercase
+  text-transform: uppercase;
 }
 td {
   padding: 10px;
@@ -60,7 +107,8 @@ td {
 tr.disabled {
   color: red;
 }
-input, select {
+input,
+select {
   font-size: 16px;
 }
 </style>
