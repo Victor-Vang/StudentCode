44465ea7a0c47f43b8fb26f393ed1e7e796f2e18
John Fulton <john@techelevator.com>
2022-04-07 14:44:24 -0400

    module-3/17_Review_Day/

---
+++ module-3/17_Review_Day/pet-info-with-johns-changes/vue/src/views/Pets.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <pet-view />
+    <pet-add />
+  </div>
+</template>
+
+<script>
+import PetAdd from "../components/PetAdd.vue";
+import PetView from "../components/PetView.vue";
+
+export default {
+  components: {
+    PetView,
+    PetAdd,
+  },
+};
+</script>
+
+<style>
+</style>
